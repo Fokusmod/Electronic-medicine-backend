cd9395892838970806eae3c91ff0a119e1cc6cf6
Fokusmod <80995808+Fokusmod@users.noreply.github.com>
2025-01-15 14:50:17 +0300

    Initial commit

---
+++ src/main/java/com/electronic/medicine/Medicine_Application.java
@@ -0,0 +1,13 @@
+package com.electronic.medicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Medicine_Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Medicine_Application.class, args);
+	}
+
+}
